f66cf6bdb28c9049d01287cdf4efbc130949e5b2
salman4ll <sfarisi0101@gmail.com>
2023-03-11 13:08:01 +0700

    p4

---
+++ p04_intent/app/src/main/java/com/example/p04_intent/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.p04_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMove = findViewById(R.id.btnMove);
+        Button btnShare = findViewById(R.id.btnShare);
+
+        btnMove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+    }
+}
